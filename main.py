ec6c8ebc31063b60eab15601f855958e6518e64a
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-06-25 14:02:49 +0300

    Thoughts on the TODO's added them.

--- main.py
+++ main.py
@@ -29,5 +29,25 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+user_choice = input('What would you like? (espresso, latte, cappuccino, report)')
 
-input('What would you like? ')
+# TODO 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):
+
+# TODO 2. turn the coffe mashine off, by typing 'off' in the promt.
+while user_choice != 'off':
+    user_choice = input('What would you like? (espresso, latte, cappuccino, report)')
+# TODO 3. print report what does the coffee mashine has in it.  water/milk/coffee/money.
+
+# TODO 4. Check if the resources in coffee mashine is sufficient for selected coffee type.
+
+# TODO 5. If resources are sufficient prompt user to insert coins. quarter - 0.25 dime 0.10 nickel - 0.05 penny - 0.01.
+
+# TODO 6. Calculate monetary value and check if >= to selected coffee price. Offer change if inserted too much.
+
+# TODO 7. If resources are sufficient and money are enough deduct the ingridients from the mashine and make coffe and add money to coffe mashine while giving user the selectedd coffee.
+
+
+
+else:
+    exit()
+

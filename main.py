c8f18e4f8796f0153943cba30168771b654c02da
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-06-29 17:19:52 +0300

    Commit

--- main.py
+++ main.py
@@ -31,11 +31,8 @@ resources = {
     "money": 0,
 }
 
-#def on_off():
-    #keep_going = input('If you would like to turn off, type "off"')
-    #return keep_going
 def choice():
-    user_choice = input('What would you like? (espresso, latte, cappuccino, report)')
+    user_choice = input('What would you like? (Espresso, Latte, Cappuccino) ')
     return user_choice
 
 # TODO 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):
@@ -47,38 +44,57 @@ def actions():
         print(resources)
     elif cho == 'espresso':
         espresso()
+    elif cho == 'latte':
+        latte()
+    elif cho == 'cappuccino':
+        cappuccino()
     elif cho == 'off':
-        print('off')
+        print('Coffee machine is now off.')
         exit()
-
 
 def espresso():
     if resources['water'] >= MENU['espresso']["ingredients"]['water'] and resources['coffee'] >= MENU['espresso']["ingredients"]['coffee']:
-        pinigai = insert_coins()
+        inserted_money = insert_coins()
         if pinigai >= MENU['espresso']['cost']:
-            change = pinigai - MENU['espresso']['cost']
+            change = inserted_money - MENU['espresso']['cost']
             resources['water'] -= MENU['espresso']["ingredients"]['water']
             resources['coffee'] -= MENU['espresso']["ingredients"]['coffee']
-            print(f'here and there is your change {change}')
+            resources['money'] += MENU['espresso']['cost']
+            print(f'Here is your Espresso and there is your change {round(change, 2)}')
         else:
-            print('insert more money')
-
+            print(f'Insert more coins, here is your: $ {inserted_money} back.')
     else:
-        print('not enough')
+        print('Not enough resources')
 
 def latte():
     if resources['water'] >= MENU['latte']["ingredients"]['water'] and resources['coffee'] >= MENU['latte']["ingredients"]['coffee'] and resources['milk'] >= MENU['latte']["ingredients"]['milk']:
-        print('here')
-
+        inserted_money = insert_coins()
+        if inserted_money >= MENU['latte']['cost']:
+            change = inserted_money - MENU['latte']['cost']
+            resources['milk'] -= MENU['latte']["ingredients"]['milk']
+            resources['water'] -= MENU['latte']["ingredients"]['water']
+            resources['coffee'] -= MENU['elatte']["ingredients"]['coffee']
+            resources['money'] += MENU['latte']['cost']
+            print(f'Here is your Latte and there is your change {round(change, 2)}')
+        else:
+            print(f'Insert more coins, here is your: $ {inserted_money} back.')
     else:
-        print('not enough')
+        print('Not enough resources')
 
 def cappuccino():
-    if resources['water'] >= MENU['cappuccino']["ingredients"]['water'] and resources['coffee'] >= MENU['cappuccino']["ingredients"]['milk'] and resources['milk'] >= MENU['cappuccino']["ingredients"]['coffee']:
-        print('here')
-
+    if resources['water'] >= MENU['cappuccino']["ingredients"]['water'] and resources['coffee'] >= MENU['cappuccino']["ingredients"]['coffee'] and resources['milk'] >= MENU['cappuccino']["ingredients"]['milk']:
+        inserted_money = insert_coins()
+        if inserted_money >= MENU['cappuccino']['cost']:
+            change = inserted_money - MENU['cappuccino']['cost']
+            resources['milk'] -= MENU['cappuccino']["ingredients"]['milk']
+            resources['water'] -= MENU['cappuccino']["ingredients"]['water']
+            resources['coffee'] -= MENU['cappuccino']["ingredients"]['coffee']
+            resources['money'] += MENU['cappuccino']['cost']
+            print(f'Here is your Cappuccino and there is your change: ${round(change, 2)}')
+        else:
+            print(f'Insert more coins, here is your: $ {inserted_money} back.')
     else:
-        print('not enough')
+        print('Not enough resources')
 def insert_coins():
     quarters = int(input('How many quarters? '))
     dimes = int(input('How many dimes? '))
@@ -87,22 +103,9 @@ def insert_coins():
     total = quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
     return total
 
-
-
-# TODO 2. turn the coffee machine off, by typing 'off' in the promt.user_choice = choice()
 going = 'on'
 
 while going != 'off':
         actions()
-        #going = on_off()
-#insert_coins()
-# TODO 3. print report what does the coffee machine has in it.  water/milk/coffee/money.
 
-# TODO 4. Check if the resources in coffee machine is sufficient for selected coffee type.
-
-# TODO 5. If resources are sufficient prompt user to insert coins. quarter - 0.25 dime 0.10 nickel - 0.05 penny - 0.01.
-
-# TODO 6. Calculate monetary value and check if >= to selected coffee price. Offer change if inserted too much.
-
-# TODO 7. If resources are sufficient and money are enough deduct the ingridients from the mashine and make coffe and add money to coffe mashine while giving user the selectedd coffee.
 
